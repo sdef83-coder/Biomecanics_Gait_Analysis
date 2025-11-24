f572fb9dfd9fdcfd8c517d738a409184c6c41362
Silvère De Freitas <silvere.defreitas@gmail.com>
2025-11-24 11:13:48 -0500

    Add normalization of Gait Speed and Step Width

--- 4) Non_Dimensional_Normalization.m
+++ 4) Non_Dimensional_Normalization.m
@@ -1,4 +1,4 @@
-%% AJOUT DE PARAMETRE DANS LE .MAT POUR SPATIO-TEMPORAL ANALYSIS, SCRIPT PRINCIPAL AUTOMATISÉ
+%% AJOUT DE PARAMETRE DANS LE .MAT POUR SPATIO-TEMPORAL ANALYSIS
 % Pas obligé de runner tout les participants
 
 clc;
@@ -37,26 +37,36 @@ for p = 1:length(participants)
         filename = [participant '_' condition '.mat'];
         
         if isfile(filename)
-            load(filename); % charge la variable 'c'
+            load(filename); % charge la structure'c'
             
-            % === Jambe droite : ajout NormStepLength et NormCadence ===
+            % === Jambe droite : ajout NormStepLength, NormCadence, NormWalkRatio, NormStepWidthHeel, et NormWalkSpeed ===
             N_right = length(c.resultsAll.kin.Right);
             for n = 1:N_right
                 distPasR = c.resultsAll.kin.Right(n).distPas;
-                cadenceR = c.resultsAll.kin.Right(n).vitCadencePasParMinute * 2;
+                cadenceR = c.resultsAll.kin.Right(n).vitCadencePasParMinute * 2; % x2 car ici cycle par minute et pas ppm
+                LargPasR = c.resultsAll.kin.Right(n).stepWidthHeel * 10; % x10 car ici la variable est en mm, on l'a met en cm
+                WalkSpeedR = c.resultsAll.kin.Right(n).vitFoulee;
+                % (Hof et al., 1996) pour les méthodes de normalisation des variables
                 c.resultsAll.kin.Right(n).NormStepLength = distPasR / l0;
                 c.resultsAll.kin.Right(n).NormCadence = (cadenceR/60) * sqrt(l0 / g);
                 c.resultsAll.kin.Right(n).NormWalkRatio = (distPasR / l0) / ((cadenceR/60) * sqrt(l0 / g));
+                c.resultsAll.kin.Right(n).NormStepWidthHeel = LargPasR / l0;
+                c.resultsAll.kin.Right(n).NormWalkSpeed = WalkSpeedR / sqrt(l0*g);
             end
             
-            % === Jambe gauche : ajout NormStepLength et NormCadence ===
+            % === Jambe gauche : ajout NormStepLength, NormCadence, NormWalkRatio, NormStepWidthHeel, et NormWalkSpeed ===
             N_left = length(c.resultsAll.kin.Left);
             for n = 1:N_left
                 distPasL = c.resultsAll.kin.Left(n).distPas;
                 cadenceL = c.resultsAll.kin.Left(n).vitCadencePasParMinute * 2;
+                LargPasL = c.resultsAll.kin.Left(n).stepWidthHeel * 10;
+                WalkSpeedL = c.resultsAll.kin.Left(n).vitFoulee;
+                % (Hof et al., 1996) pour les méthodes de normalisation des variables
                 c.resultsAll.kin.Left(n).NormStepLength = distPasL / l0;
                 c.resultsAll.kin.Left(n).NormCadence = (cadenceL/60) * sqrt(l0 / g);
                 c.resultsAll.kin.Left(n).NormWalkRatio = (distPasL / l0) / ((cadenceL/60) * sqrt(l0 / g));
+                c.resultsAll.kin.Left(n).NormStepWidthHeel = LargPasL / l0;
+                c.resultsAll.kin.Left(n).NormWalkSpeed = WalkSpeedL / sqrt(l0*g);
             end
             
             % === Sauvegarde du fichier avec les données ajoutées ===
